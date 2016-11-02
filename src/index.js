f85f73900952b2a698b3f961079cb977b624bfda
HONG ZHOU <holly_zhou@yahoo.com>
2016-11-01 20:22:22 -0700

    add eslint

--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from 'material-ui';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import store from './store';
-
 import Layout from './components/Layout';
 // import HomePage from './components/HomePage';
+
+injectTapEventPlugin();
 
 
 render(
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={Layout}>
-        {/* <IndexRoute component={HomePage} /> */}
+    <MuiThemeProvider>
+      <Router history={browserHistory}>
+        <Route path="/" component={Layout}>
+          {/* <IndexRoute component={HomePage} /> */}
 
-      </Route>
-    </Router>
+        </Route>
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
